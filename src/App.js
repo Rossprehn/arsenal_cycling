060cd7672ee0328b2a70dae5d810170411f3169c
Rossprehn <32393462+Rossprehn@users.noreply.github.com>
2018-05-22 14:35:53 -0600

    path working

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import Main from './Main'
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-          <Route exact path="/" render={() => <Redirect to="/main" />} />
+          <Route exact path="/" render={() => <Main />} />
         </div>
       </Router>
     )
