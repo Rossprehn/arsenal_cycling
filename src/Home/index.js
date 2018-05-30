4308c67362f6ef7be0f4a34f90b2dfb785bdeed2
Paul Highum <phigh07@gmail.com>
2018-05-30 14:41:32 -0600

    product container

--- src/Home/index.js
+++ src/Home/index.js
@@ -5,9 +5,15 @@ import './style.css'
 class Home extends Component {
   render() {
     return (
-      <div className="hero-img">
-        <img />
-        <p>No Ordinary Light</p>
+      <div>
+        <div className="hero-img">
+          <img />
+          <p>No Ordinary Light</p>
+        </div>
+        <div className="product-container">
+          <h3>Our Product</h3>
+          <h4>A powerful combination of cycling, technology, and safety</h4>
+        </div>
       </div>
     )
   }
